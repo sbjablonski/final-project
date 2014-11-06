2e902d17aa3aed92220fee1f368c030e6b946cf5
Stephen Jablonski <stephenbjablonski@gmail.com>
2014-11-06 07:40:59 -0600

    Full menu
    Finished the menu, need to rewrite for two arrows, and design to go up
    and down. Also change cans out of divs to animate separate.

--- js/beer-menu.js
+++ js/beer-menu.js
@@ -1,44 +1,79 @@
 // On click, //
 
 
-$('#beer-left').on('click',function(){
-    if($('.beer-type:first').is('#rodeoclown') != 'images/sympathy.png'){
+// $('#beer-left').on('click',function(){
 
-        $('.beer-description').html('<p>Please allow me to introduce myself Im a beer of damn fine taste. Ive been around for a long, long time, Many brewers have ruined my name Ive watched now for several decades. As my character has been disgraced, But now the boys down on Karbach Street Have gone and made me first rate.<br><br> Full of fine malt and German hops, My taste is both clean and bold And though my flavor always stands up Im best when enjoyed cold So if you meet me Have some courtesy Show some sympathy Enjoy my taste.</p>');
+//         $('#rodeoclown').toggleClass('beer-display');
+//         $('#sympathy').toggleClass('beer-display');
+//     if($('#sympathy').hasClass('beer-display') != true){
+//         $('body').css('background-color','#c4b5ad');
+//         $('header').css('background-color','#ed4634');
+//         $('footer').css('background-color','#ed4634');
+//         $('#sympathy p').css('color','#333');
+//         $('#sympathy li').css('color','#333');
+//         $('#sympathy .fa-video-camera').css('color','#333');
+//     }
+//     else if ($('#rodeoclown').hasClass('beer-display') != true){
+//         $('body').css('background-color','#f0302b');
+//         $('header').css('background-color','#282c89');
+//         $('footer').css('background-color','#282c89');
+//         $('p').css('color','#fff');
+//         $('li').css('color','#fff');
+//    }
 
-//CSS Changes//
+// });
+
+// BEER TOGGLE - When arrow is clicked, hide current beer-type and show next beer in the array. //
+
+    var beersarray = ["rodeoclown","sympathy","hopadillo","weekendwarrior","weisseversa","mother"];
+
+    var beercount = 1; // Start at "sympathy" in the array (since "rodeoclown" loads with the page). //
+
+    $('#beer-left').on('click',function(){
+        var beerid = '#' + beersarray[beercount]; // When arrow clicked, set the value in array at current count.
+        var beeridprev = '#' + beersarray[beercount - 1]; // Set the value in array at previous count. //
+        $(beeridprev).toggleClass('beer-display'); // Toggle previous count (current displayed) beer-type to 'none.'
+        $(beerid).toggleClass('beer-display'); // Toggle the current count (next to show) beer-type to display.
+        beercount += 1; // Set count for next click. //
+        console.log(beerid);
+        if (beercount == beersarray.length){ // Reset count when the end of the array is reached to select through array again, starting at "rodeoclown." //
+            beercount = 0;
+        }
+
+// COLORS //
+        if (beerid == "#rodeoclown"){
+            $("#mother").addClass('beer-display'); //resets beer-display so "mother" doesn't continue in next cycle.
+            $('body').css('background-color','#f0302b');
+            $('header').css('background-color','#282c89');
+            $('footer').css('background-color','#282c89');
+        };
+        if (beerid == "#sympathy"){
+            $('body').css('background-color','#c4b5ad');
+            $('header').css('background-color','#ed4634');
+            $('footer').css('background-color','#ed4634');
+        };
+        if (beerid == "#hopadillo"){
+            $('body').css('background-color','#97dca4');
+            $('header').css('background-color','#573a53');
+            $('footer').css('background-color','#573a53');
+        };
+        if (beerid == "#weekendwarrior"){
+            $('body').css('background-color','#35e181');
+            $('header').css('background-color','#145444');
+            $('footer').css('background-color','#145444');
+        };
+        if (beerid == "#weisseversa"){
+            $('body').css('background-color','#b9a22f');
+            $('header').css('background-color','#0094e1');
+            $('footer').css('background-color','#0094e1');
+        };
+        if (beerid == "#mother"){
+            $('body').css('background-color','#784e52');
+            $('header').css('background-color','#202c39');
+            $('footer').css('background-color','#202c39');
+        };
+    });
 
 
-        $('#beer-img').attr('src','images/sympathy.png');
-        $('body').css('background-color','#c4b5ad');
-        $('header').css('background-color','#ed4634');
-        $('footer').css('background-color','#ed4634');
-    }else if($('#beer-img').attr('src') == 'images/sympathy.png'){
-        $('#beer-img').attr('src','images/rodeoclown.png');
-        $('body').css('background-color','#F0302B');
-        $('header').css('background-color','#282c89');
-        $('footer').css('background-color','#282c89');
-    }
-});
 
-//Same, for right arrow//
 
-$('#beer-right').on('click',function(){
-    if($('#rodeoclown').css('display')!='none'){
-
-        $('.beer-description').html('<p>Please allow me to introduce myself Im a beer of damn fine taste. Ive been around for a long, long time, Many brewers have ruined my name Ive watched now for several decades. As my character has been disgraced, But now the boys down on Karbach Street Have gone and made me first rate.<br><br> Full of fine malt and German hops, My taste is both clean and bold And though my flavor always stands up Im best when enjoyed cold So if you meet me Have some courtesy Show some sympathy Enjoy my taste.</p>');
-
-        $('#beer-img').animate({
-            width: [ "toggle", "swing" ]
-            }, 500, "easein", function() {
-                $('#beer-img').attr('src','images/sympathy.png');
-                });
-        $('body').css('background-color','#c4b5ad');
-        $('header').css('background-color','#ed4634');
-        $('footer').css('background-color','#ed4634');
-        $('p').css('color','#333');
-    }else if($('#2').css('display')!='none'){
-        $('#1').show().siblings('div').hide();
-    }
-    console.log("test.");
-});
