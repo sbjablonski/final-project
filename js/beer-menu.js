159586db9d6d1e4b47493007582c1c7b1914db87
Stephen Jablonski <stephenbjablonski@gmail.com>
2014-11-10 23:24:57 -0600

    Design changes
    Also added beer lineup menu.

--- js/beer-menu.js
+++ js/beer-menu.js
@@ -37,157 +37,92 @@
 $('#beer-right').on('click', function(){
         if ($('#rodeoclown').hasClass('no-display') != true){
             $(beerlast).addClass('no-display');
-            $('body').css('background-color','#f0302b');
+            $('.logo-cont').css('background-color','#f0302b');
             $('header').css('background-color','#282c89');
+            $('footer').css('border-top','10px solid #f0302b');
             $('footer').css('background-color','#282c89');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
         };
 
         if ($('#sympathy').hasClass('no-display') != true){
-            $('body').css('background-color','#c4b5ad');
-            $('header').css('background-color','#ed4634');
-            $('footer').css('background-color','#ed4634');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+            $('.logo-cont').css('background-color','#c4b5ad');
+            $('header').css('background-color','#b22f11');
+            $('footer').css('border-top','10px solid #c4b5ad');
+            $('footer').css('background-color','#b22f11');
+
         };
         if ($('#hopadillo').hasClass('no-display') != true){
-            $('body').css('background-color','#97dca4');
+            $('.logo-cont').css('background-color','#97dca4');
             $('header').css('background-color','#573a53');
+            $('footer').css('border-top','10px solid #97dca4');
             $('footer').css('background-color','#573a53');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+
         };
         if ($('#weekendwarrior').hasClass('no-display') != true){
-            $('body').css('background-color','#35e181');
+            $('.logo-cont').css('background-color','#35e181');
             $('header').css('background-color','#145444');
+            $('footer').css('border-top','10px solid #35e181');
             $('footer').css('background-color','#145444');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+
         };
         if ($('#weisseversa').hasClass('no-display') != true){
-            $('body').css('background-color','#b9a22f');
+            $('.logo-cont').css('background-color','#f9dd15');
             $('header').css('background-color','#0094e1');
+            $('footer').css('border-top','10px solid #f9dd15');
             $('footer').css('background-color','#0094e1');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+
         };
         if ($('#mother').hasClass('no-display') != true){
-            $('body').css('background-color','#784e52');
+            $('.logo-cont').css('background-color','#784e52');
             $('header').css('background-color','#202c39');
+            $('footer').css('border-top','10px solid #784e52');
             $('footer').css('background-color','#202c39');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
-            $(rodeoclown).addClass('no-display');
+            $('#rodeoclown').addClass('no-display');
         };
     });
 
 $('#beer-left').on('click', function(){
         if ($('#rodeoclown').hasClass('no-display') != true){
             $(beerlast).addClass('no-display');
-            $('body').css('background-color','#f0302b');
+            $('.logo-cont').css('background-color','#f0302b');
             $('header').css('background-color','#282c89');
+            $('footer').css('border-top','10px solid #f0302b');
             $('footer').css('background-color','#282c89');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+
         };
 
         if ($('#sympathy').hasClass('no-display') != true){
-            $('body').css('background-color','#c4b5ad');
-            $('header').css('background-color','#ed4634');
-            $('footer').css('background-color','#ed4634');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+            $('.logo-cont').css('background-color','#c4b5ad');
+            $('header').css('background-color','#b22f11');
+            $('footer').css('border-top','10px solid #c4b5ad');
+            $('footer').css('background-color','#b22f11');
+
         };
         if ($('#hopadillo').hasClass('no-display') != true){
-            $('body').css('background-color','#97dca4');
+            $('.logo-cont').css('background-color','#97dca4');
             $('header').css('background-color','#573a53');
+            $('footer').css('border-top','10px solid #97dca4');
             $('footer').css('background-color','#573a53');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+
         };
         if ($('#weekendwarrior').hasClass('no-display') != true){
-            $('body').css('background-color','#35e181');
+            $('.logo-cont').css('background-color','#35e181');
             $('header').css('background-color','#145444');
+            $('footer').css('border-top','10px solid #35e181');
             $('footer').css('background-color','#145444');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+
         };
         if ($('#weisseversa').hasClass('no-display') != true){
-            $('body').css('background-color','#b9a22f');
+            $('.logo-cont').css('background-color','#f9dd15');
             $('header').css('background-color','#0094e1');
+            $('footer').css('border-top','10px solid #f9dd15');
             $('footer').css('background-color','#0094e1');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+
         };
         if ($('#mother').hasClass('no-display') != true){
-            $('body').css('background-color','#784e52');
+            $('.logo-cont').css('background-color','#784e52');
             $('header').css('background-color','#202c39');
+            $('footer').css('border-top','10px solid #784e52');
             $('footer').css('background-color','#202c39');
-            $(rodeoclown).addClass('no-display');
-            $('p').css('color','#fff');
-            $('li').css('color','#fff');
-            $('.fa').css('color','#fff');
+            $('#rodeoclown').addClass('no-display');
         };
     });
-
-// // ON SCROLL BEER TOGGLE - All beer types displayed, changes colors when scrolled to corresponding beer type.
-
-//     var beersarray = ["rodeoclown","sympathy","hopadillo","weekendwarrior","weisseversa","mother"];
-
-//     var beercount = 1; // Start at "sympathy" in the array (since "rodeoclown" loads with the page). //
-
-//         for (var i = beercount; i < beersarray.length; i++){
-//             var beerid = '#' + beersarray[i];
-//             $(beerid).toggleClass('no-display');
-//             $(beerid).css('margin-top','200px');
-//             console.log(beerid);
-//         };
-
-//     $(window).on('scroll',function(){
-//         if ($('#rodeoclown').visible() == true){
-//             $('body').css('background-color','#f0302b');
-//             $('header').css('background-color','#282c89');
-//             $('footer').css('background-color','#282c89');
-//         };
-//         if ($('#sympath').visible() == true){
-//             $('body').css('background-color','#c4b5ad');
-//             $('header').css('background-color','#ed4634');
-//             $('footer').css('background-color','#ed4634');
-//         };
-//         if ($('#hopadillo').visible() == true){
-//             $('body').css('background-color','#97dca4');
-//             $('header').css('background-color','#573a53');
-//             $('footer').css('background-color','#573a53');
-//         };
-//         if ($('#weekendwarrior').visible() == true){
-//             $('body').css('background-color','#35e181');
-//             $('header').css('background-color','#145444');
-//             $('footer').css('background-color','#145444');
-//         };
-//         if ($('#weisseversa').visible() == true){
-//             $('body').css('background-color','#b9a22f');
-//             $('header').css('background-color','#0094e1');
-//             $('footer').css('background-color','#0094e1');
-//         };
-//         if ($('#mother').visible() == true){
-//             $('body').css('background-color','#784e52');
-//             $('header').css('background-color','#202c39');
-//             $('footer').css('background-color','#202c39');
-//         };
-//     });
-
-
-
-
